00871e5813a386676780777a23e62e84325ad6e5
marcos lott <lott.marcos@gmail.com>
2023-02-15 20:22:23 -0300

    updating TimeControl decorator

--- app/src/decorators/TimeControl.ts
+++ app/src/decorators/TimeControl.ts
@@ -1,15 +1,21 @@
-export function TimeControl() {
+export function TimeControl(isSeconds: boolean = false) {
   return function (
     target: any,
     propertyKey: string,
     descriptor: PropertyDescriptor
   ) {
     const currentMethod = descriptor.value;
+    let divisor = 1;
+    let timeUnit = "miliseconds";
+    if (isSeconds) {
+      divisor = 1000;
+      timeUnit = "seconds";
+    }
     descriptor.value = function (...args: any[]) {
       const t1 = performance.now();
       const methodReturn = currentMethod.apply(this, args);
       const t2 = performance.now();
-      console.log(`${propertyKey}: ${(t2 - t1) / 1000} seconds`);
+      console.log(`${propertyKey}: ${(t2 - t1) / divisor} ${timeUnit}`);
       methodReturn;
     };
     return descriptor;
